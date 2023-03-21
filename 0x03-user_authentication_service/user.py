e97cca03bce9f941b2ff2554f3091a92e2a99e88
JumaOchi <ochiengjuma95@gmail.com>
2023-03-21 23:09:58 +0000

    pycodestyle fix correction

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -11,7 +11,6 @@ Base = declarative_base()
 class User(Base):
     """model base class"""
     __tablename__ = 'users'
-
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable = False)
     hashed_password = Column(String(250), nullable=False)
